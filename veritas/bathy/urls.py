1b5e499034e7383ed04b8913d9281e5a789fb118
unknown <Shang,Zhendan>
2025-04-06 17:24:45 +0100

    website add pages and login/register

--- veritas/bathy/urls.py
+++ veritas/bathy/urls.py
@@ -3,4 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('case_studies', views.case_studies, name='case_studies'),
+    path('benchmarking', views.benchmarking, name='benchmarking'),
+    path('product_offering', views.product_offering, name='product_offering'),
+    path('contact_us', views.contact_us, name='contact_us'),
+    path('login', views.login, name='login'),
+    path('register', views.register, name='register'),
 ]
--- veritas/bathy/views.py
+++ veritas/bathy/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 from django.http import HttpResponse
@@ -6,3 +7,37 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request, 'index.html')
+
+def case_studies(request):
+    return render(request, 'case_studies.html')
+
+def benchmarking(request):
+    return render(request, 'benchmarking.html')
+
+def product_offering(request):
+    return render(request, 'product_offering.html')
+
+def contact_us(request):
+    return render(request, 'contact_us.html')
+
+def login(request):
+
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('index')
+    return render(request, 'login.html')
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'register.html', {'form': form})
